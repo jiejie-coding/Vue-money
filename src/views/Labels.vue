1bf91a6bd2cf88821712e307bd3075d0563130f9
jiejie <189425098@qq.com>
2021-01-06 11:44:57 +0800

    页面布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    Labels.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Labels.vue
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +40,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    border: 1px solid blue;
+  }
+  .content {
+    border: 1px solid red;
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Statistics.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Statistics.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +14,15 @@ name: "Statistics",
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  border: 1px solid blue;
+}
+.content {
+  border: 1px solid red;
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
